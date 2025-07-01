d197da1618b4c6c0f7b7cc8bf8c22ef1d1ff8243
sanjidTalukder <sanjidtalukder02@gmail.com>
2025-07-01 06:03:30 +0600

    This commit for index modify

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("✅ Successfully connected to MongoDB!");
 
     const db = client.db("user");
